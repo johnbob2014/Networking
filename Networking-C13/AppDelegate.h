55d206ebc13a960ebe0cb4a109d085b95998d81f
johnbob2014 <johnbob2014@icloud.com>
2016-06-02 17:31:41 +0800

    Initial Commit

---
+++ Networking-C13/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Networking-C13
+//
+//  Created by BobZhang on 16/6/2.
+//  Copyright © 2016年 BobZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
